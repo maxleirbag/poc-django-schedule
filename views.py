057e4dd973a024655d4c2dca33ae85b09b85364f
Max <78800453+maxleirbag@users.noreply.github.com>
2021-08-08 20:55:42 -0300

    Erased non-sense commented code

--- views.py
+++ views.py
@@ -1,13 +1,6 @@
 from django.shortcuts import render, HttpResponse
-#from core.models import Pessoa
 # Create your views here.
 
+# In case the user doesn't know an specific address, he/she is greeted
 def hello(requests,name="world",age=3500000000):
     return HttpResponse(f"<h1>Hello, {name}!</h1><h2>I'm aware that you are {age} years old. </h2><h3>Watch out... 👀🐱‍👤 </h3>")
-
-
-# class PessoaAdmin(admin.modelAdmin):
-#     list_display = ('name','idade')
-#     list_filter = ('name')
-#
-# admin.site.register(Pessoa)
