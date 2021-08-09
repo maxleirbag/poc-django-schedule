70dc3da260e6e73ba7b602a0a6f6dc753268aedb
Max <78800453+maxleirbag@users.noreply.github.com>
2021-08-09 01:38:16 -0300

    Delete admin.py

--- admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from core.models import Event
-# Register your models here.
-
-
-class AdminEvent(admin.ModelAdmin):
-    list_display = ('title','event_date','creation_date')
-    list_filter = ('title','users','event_date')
-admin.site.register(Event, AdminEvent)
